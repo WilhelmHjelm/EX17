5837a5968a1838e7fec363ee6f90e068a69efcab
Wilhelm Hjelm <wilhelm@hjelms.se>
2017-01-13 15:35:52 +0100

    THE CREATION

---
+++ footer.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package EX17
+ */
+
+?>
+
+	</div><!-- #content -->
+
+	<footer id="colophon" class="site-footer" role="contentinfo">
+		<div class="site-info">
+			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'ex17' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'ex17' ), 'WordPress' ); ?></a>
+			<span class="sep"> | </span>
+			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'ex17' ), 'ex17', '<a href="https://automattic.com/" rel="designer">Wilhelm Hjelm & Patrik Grändevall</a>' ); ?>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
