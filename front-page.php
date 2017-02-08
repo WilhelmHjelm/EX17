5960c4753800e1b07262caa4d752ad2940a4d91d
Wilhelm Hjelm <wilhelm@hjelms.se>
2017-02-08 13:07:46 +0100

    Let's get this shit started

---
+++ front-page.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * The front-page of the theme
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package EX17
+ */ get_header(); ?>
+
+<div class="container">
+  <div class="four columns">
+    <h2>11-13 maj</h2>
+    <h3>Färgeriet</h3>
+  </div>
+  <div class="eight columns">
+    FÖRELÄSARE
+  </div>
+</div>
+<div class="container">
+
+</div>
+
+
+
+<?php// get_footer(); ?>
--- header.php
+++ header.php
@@ -16,34 +16,44 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="profile" href="http://gmpg.org/xfn/11">
 
+<meta charset="<?php bloginfo( 'charset' ); ?>">
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+<meta description="Examensdagarna för Grafisk design och kommunikation, 12-14 maj i Kåkenhus på Campus Norrköping.">
+<meta property="og:site_name" content="EX16">
+<?php $current_url="http://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']; ?>
+<meta property="og:url" content="<?php echo $current_url; ?>">
+<meta property="og:title" content="<?php if ( is_front_page() && is_home() ) : ?>Startsida<?php else : echo the_title(); endif; ?>">
+<meta property="og:description" content="Examensdagarna för Grafisk design och kommunikation, 11-13 maj i Kåkenhus på Campus Norrköping.">
+<meta property="og:image" content="<?php echo get_stylesheet_directory_uri(); ?>/img/logo_social_1200x630.png">
+
 <?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
 <div id="page" class="site">
-	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'ex17' ); ?></a>
 
 	<header id="masthead" class="site-header" role="banner">
-		<div class="site-branding">
-			<?php
-			if ( is_front_page() && is_home() ) : ?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-			<?php else : ?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-			<?php
-			endif;
+		<div class="container">
+			<nav class="main-nav twelve columns">
+				<?php
+				if ( is_front_page() && is_home() ) : ?>
+						<img src="logo">
+						<?php wp_nav_menu( array( 'theme_location' => 'main-nav', 'menu_id' => 'main-nav' ) ); ?>
+					</nav>
+					<div class="twelve columns">
+						FIRST BOX
+					</div>
+				<?php else : ?>
+					<img src="logo">
+					<?php wp_nav_menu( array( 'theme_location' => 'main-nav', 'menu_id' => 'main-nav' ) ); ?>
+				</nav>
+				<div class="twelve columns">
+					FIRST BOX
+				</div>
+				<?php
 
-			$description = get_bloginfo( 'description', 'display' );
-			if ( $description || is_customize_preview() ) : ?>
-				<p class="site-description"><?php echo $description; /* WPCS: xss ok. */ ?></p>
-			<?php
-			endif; ?>
-		</div><!-- .site-branding -->
-
-		<nav id="site-navigation" class="main-navigation" role="navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'ex17' ); ?></button>
-			<?php wp_nav_menu( array( 'theme_location' => 'menu-1', 'menu_id' => 'primary-menu' ) ); ?>
-		</nav><!-- #site-navigation -->
+				endif; ?>
+		</div>
 	</header><!-- #masthead -->
 
 	<div id="content" class="site-content">
