d558238a0fb0fa50a2edbb9e7e703d97bc6177ba
Wilhelm Hjelm <wilhelm@hjelms.se>
2017-02-09 18:39:36 +0100

    test of both flexbox and skeleton for grid
    vet inte vad som blir bäst asså, har fuskat på båda

--- front-page.php
+++ front-page.php
@@ -7,7 +7,7 @@
  * @package EX17
  */ get_header(); ?>
 
-<div class="container">
+<!--<div class="container">
   <div class="four columns box" id="box-1">
     <h2>11-13 maj</h2>
     <h3>Färgeriet</h3>
@@ -18,8 +18,55 @@
 </div>
 <div class="container">
 
+</div> -->
+
+<div class="container">
+  <div class="flexbox-grid">
+      <div class="flexbox-box" id="box-1">
+        <div class="flexbox-content" id="box-1-content">
+          <h1>11-13 maj</h1>
+          <h3>Färgeriet</h3>
+        </div>
+      </div>
+      <div class="flexbox-box" id="box-2">
+        HEJ 2
+      </div>
+      <div class="flexbox-box" id="box-3">
+        HEJ 3
+      </div>
+
+      <div class="flexbox-box" id="box-5">
+        HEJ 5
+      </div>
+      <div class="flexbox-box" id="box-6">
+        <div class="flexbox-content">
+          HEJ 6
+        </div>
+      </div>
+      <div class="flexbox-box" id="box-4">
+        HEJ 4
+      </div>
+  </div>
 </div>
 
+<div class="container">
+  <div class="three columns" id="box1">
+  </div>
+  <div class="nine columns" id="box2">
+  </div>
+  <div class="eight columns" id="box3">
+  </div>
+  <div class="four columns" id="box4">
+  </div>
+  <div class="four columns" id="box5">
+  </div>
+  <div class="four columns" id="box6">
+  </div>
+  <div class="six columns" id="box7">
+  </div>
+  <div class="six columns" id="box8">
+  </div>
+</div>
 
 
 <?php// get_footer(); ?>
