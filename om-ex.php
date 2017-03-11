0714897fd1caa385d1ad7ce45806583422fd85c9
Wilhelm Hjelm <wilhelm@hjelms.se>
2017-03-11 13:40:09 +0100

    implementation of font awesome and start of about-page

--- om-ex.php
+++ om-ex.php
@@ -9,10 +9,39 @@ get_header(); ?>
 
 <div class="container">
 
+<div class="row">
+
+<div class="seven columns about-p">
+
   <?php while ( have_posts() ) : the_post(); ?>
 
     <?php the_content(); ?>
 
   <?php endwhile; // end of the loop. ?>
+
+</div>
+
+<div class="five columns info-box">
+  <div class="info-box-content yellow-text">
+    <h2>11 - 13 maj</h2>
+    <h4><i class="fa fa-map-marker" aria-hidden="true"></i>  Färgeriet</h4>
+  </div>
+</div>
+</div>
+
+<div class="row">
+  <div class="twelve columns">
+    GOOGLE MAP
+  </div>
+</div>
+
+<div class="row">
+  <div class="seven columns about-p">
+    <?php the_field('om-projektgruppen'); ?>
+  </div>
+  <div class="five columns photo">
+
+  </div>
+</div>
 
 </div>
