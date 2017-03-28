419ed3c3ac76f7711bfa7d1ed58ae5a7b2dd297c
patrikgrandevall <Patrik@Patriks-MacBook-Pro.local>
2017-03-28 11:14:43 +0200

    footerstyle och lite mer schema

--- footer.php
+++ footer.php
@@ -79,6 +79,13 @@
 
 		<div class="twelve columns real-footer">
 			<a href="<?php echo get_home_url(); ?>"><div class="one columns"><img src="<?php echo get_template_directory_uri();?>/img/logo-text.svg" alt="Logo" height="70px"></div></a>
+			<div class="footer-icons">
+				<a href="#"><i class="fa fa-envelope" aria-hidden="true"></i></a>
+				<a href="#"><i class="fa fa-linkedin" aria-hidden="true"></i></a>
+				<a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+				<a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+
+			</div>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 	</div><!-- container -->
