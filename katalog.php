5391e615b50f495a3277fac20d36ebb59561603e
patrikgrandevall <Patrik@Patriks-MacBook-Pro.local>
2017-04-04 13:56:41 +0200

    container katalog

--- katalog.php
+++ katalog.php
@@ -6,11 +6,13 @@
  */
 
 get_header(); ?>
+<div class="container">
+
 
 <?php while ( have_posts() ) : the_post(); ?>
 
   <?php the_content(); ?>
 
 <?php endwhile; // end of the loop. ?>
-
+</div>
 <?php get_footer(); ?>
